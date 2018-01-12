16fe9b568e98954737e5eb541425c0a239084f71
legendddhgf <legendddhgf@gmail.com>
2018-01-12 10:17:25 -0800

    forgot to update directories

--- pa1/pa1.sh
+++ pa1/pa1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# cmps101-pt.f17 grading
+# cmps012b-pt.w18 grading
 # usage: pa1.sh
 # (run within your pa1 directory to test your code)
 
-SRCDIR=https://raw.githubusercontent.com/legendddhgf/cmps101-pt.f17.grading/master/pa1
+SRCDIR=https://raw.githubusercontent.com/legendddhgf/cmps012b-pt.w18.grading/master/pa1
 EXE="pa1-check.sh"
 SCRIPT=$(mktemp)
 
