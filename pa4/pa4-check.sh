a6c54a6cd2bab40e0cc84d8359d02d4a3daeff70
legendddhgf <legendddhgf@gmail.com>
2018-02-11 00:41:18 -0800

    pa4 initial commit

---
+++ pa4/pa4-check.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/bash
+
+echo "Tests not yet ready"
+exit
+
+SRCDIR=https://raw.githubusercontent.com/legendddhgf/cmps012b-pt.w18.grading/master/pa4
+NUMTESTS=10
+PNTSPERTEST=X
+let MAXPTS=$NUMTESTS*$PNTSPERTEST
+
+if [ ! -e backup ]; then
+  echo "WARNING: a backup has been created for you in the \"backup\" folder"
+  mkdir backup
+fi
+
+cp *.java Makefile backup   # copy all files of importance into backup
+
+for NUM in $(seq 1 $NUMTESTS); do
+  curl $SRCDIR/infile$NUM.txt > infile$NUM.txt
+  curl $SRCDIR/model-rpt$NUM.txt > model-tpt$NUM.txt
+  curl $SRCDIR/model-trc$NUM.txt > model-trc$NUM.txt
+done
+
+curl $SRCDIR/ModelQueueTest.java > ModelQueueTest.java
+
+echo ""
+echo ""
+
+make
+
+if [ ! -e Simulation ] || [ ! -x Simulation ]; then # exist and executable
+  echo ""
+  echo "Makefile doesn't correctly create Executable!!!"
+  echo ""
+  rm -f *.class
+  javac -Xlint *.java
+  echo "Main-class: Simulation" > Manifest
+  jar cvfm Simulation Manifest *.class
+  rm Manifest
+  chmod +x Simulation
+fi
+
+echo ""
+echo ""
+
+simulationtestspassed=$(expr 0)
+echo "Please be warned that the following tests discard all output to stdout/stderr"
+echo "Simulation tests: If nothing between '=' signs, then test is passed"
+echo "Press enter to continue"
+read verbose
+for NUM in $(seq 1 $NUMTESTS); do
+  rm -f outfile$NUM.txt
+  timeout 5 Simulation infile$NUM.txt &> garbage >> garbage
+  diff -bBwu infile$NUM.txt.rpt model-rpt$NUM.txt &> diff-rpt$NUM.txt >> diff-rpt$NUM.txt
+  diff -bBwu infile$NUM.txt.trc model-trc$NUM.txt &> diff-trc$NUM.txt >> diff-trc$NUM.txt
+  echo "Report $NUM Test:"
+  echo "=========="
+  cat diff-rpt$NUM.txt
+  echo "=========="
+  echo "Trace $NUM Test:"
+  echo "=========="
+  cat diff-trc$NUM.txt
+  echo "=========="
+
+  cat diff-rpt$NUM.txt diff-trc$NUM.txt > diff$NUM.txt
+
+  if [ -e diff$NUM.txt ] && [[ ! -s diff$NUM.txt ]]; then
+    let simulationtestspassed+=1
+  fi
+
+  rm -f infile$NUM *outfile$NUM* diff*$NUM
+
+done
+
+echo ""
+echo ""
+
+let simulationtestpoints=$PNTSPERTEST*$simulationtestspassed
+
+echo "Passed $simulationtestspassed / $NUMTESTS Simulation tests"
+echo "This gives a total of $simulationtestpoints / $MAXPTS points"
+
+echo ""
+echo ""
+
+make clean
+
+if [ -e Simulation ] || [ -e *.class ]; then
+  echo "WARNING: Makefile didn't successfully clean all files"
+fi
+
+echo ""
+echo ""
+
+echo "Press Enter To Continue with QueueTest Results"
+read verbose
+
+javac *.java >> garbage &>> garbage
+cat garbage
+
+timeout 5 java ModelQueueTest -v > QueueTest-out.txt &>> QueueTest-out.txt
+cat QueueTest-out.txt
+
+
+rm -f *out.txt *Work.txt
+
+rm -f *.class ModelQueueTest.java garbage*
